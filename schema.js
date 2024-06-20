c00421e06756c213e4b226bd02bddc04b43e9144
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-06-20 16:15:39 +0545

    query variables

--- schema.js
+++ schema.js
@@ -16,9 +16,33 @@ export const typeDefs = `#graphql
   }
   type Query{
     reviews:[Review]
+    review(id:ID!):Review
     games:[Game]
+    game(id:ID!):Game
     authors:[Author]
+    author(id:ID!):Author
   }
 `;
 
 //int,float,string,boolean,ID(key for objects)
+
+//query from apollo
+/*
+query gamesQuery {
+reviews {
+  content,
+  rating
+}
+}
+
+query reviewQuery($id:ID!) {
+review(id:$id){
+  content,
+  rating
+}
+}
+in variables section
+{
+  "id": "1"
+}
+*/
