dc71dcf8e2d4cb4afe4ba5e5de7dbf0b5b8031b3
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-06-20 15:38:15 +0545

    schema

---
+++ schema.js
@@ -0,0 +1,24 @@
+export const typeDefs = `#graphql
+  type Game{
+    id:ID! # ! rep required
+    title:String!
+    platform:[String!]! #array
+  }
+  type Review{
+    id:ID!
+    rating:String!
+    content:String!
+  }
+  type Author{
+    id:ID!
+    name:String!
+    verified:Boolean!
+  }
+  type Query{
+    review:[Review]
+    game:[Game]
+    author:[Author]
+  }
+`;
+
+//int,float,string,boolean,ID(key for objects)
